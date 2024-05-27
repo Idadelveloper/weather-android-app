2dc372fa8c5dfcfb572360c9c1ca5b5ddd0898cb
Ida Delphine <idadelm@gmail.com>
2024-05-27 18:09:46 +0100

    request for location coordinates

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.weatherapp
 
+import android.annotation.SuppressLint
 import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import android.location.LocationManager
 import android.net.Uri
 import android.os.Bundle
+import android.os.Looper
 import android.provider.Settings
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -15,22 +17,64 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
 
 class MainActivity : AppCompatActivity() {
     private val REQUEST_LOCATION_CODE = 123123
+    private lateinit var mFusedLocationClient: FusedLocationProviderClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         if (!isLocationEnabled()) {
 
-            Toast.makeText(this@MainActivity, "The location is not turned enabled", Toast.LENGTH_SHORT).show()
+            Toast.makeText(
+                this@MainActivity,
+                "The location is not turned enabled",
+                Toast.LENGTH_SHORT
+            ).show()
 
             val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
             startActivity(intent)
         } else {
             requestPermissions()
         }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_LOCATION_CODE && grantResults.size > 0) {
+            Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
+            requestLocationData()
+        } else {
+            Toast.makeText(this, "The permission was not Granted", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun requestLocationData() {
+        val locationRequest = LocationRequest.Builder(
+            Priority.PRIORITY_HIGH_ACCURACY, 1000
+        ).build()
+        mFusedLocationClient.requestLocationUpdates(locationRequest, object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "latitude: ${locationResult.lastLocation?.latitude} \n longitude: ${locationResult.lastLocation?.longitude}",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }, Looper.myLooper())
     }
 
     private fun isLocationEnabled(): Boolean {
@@ -42,14 +86,16 @@ class MainActivity : AppCompatActivity() {
 
     private fun requestPermissions() {
         if (ActivityCompat.shouldShowRequestPermissionRationale(
-            this,
-            android.Manifest.permission.ACCESS_FINE_LOCATION
-        )) {
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        ) {
             showRequestDialog()
         } else if (ActivityCompat.shouldShowRequestPermissionRationale(
-            this,
-            android.Manifest.permission.ACCESS_COARSE_LOCATION
-        )) {
+                this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        ) {
             requestPermissions()
         } else {
             ActivityCompat.requestPermissions(
@@ -74,7 +120,7 @@ class MainActivity : AppCompatActivity() {
                 } catch (e: ActivityNotFoundException) {
                     e.printStackTrace()
                 }
-            }.setNegativeButton("CLOSE") {dialog, _ ->
+            }.setNegativeButton("CLOSE") { dialog, _ ->
                 dialog.cancel()
             }.setTitle("Location permission needed")
             .setMessage("Thsi permission is needed for accessing the location. it can be enabled under the Application Settings")
